293bedb59dcbe7df8ff3e76aa3145f093ea46d1e
patrickatcw <pj8392@yahoo.com>
2018-05-04 18:46:01 -0400

    seatcopy and reserve

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,5 +1,6 @@
 package com.me;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //initial code from JavaCollectionsAndBinarySearch
@@ -10,6 +11,25 @@ public class Main {
 
         //starting steps for collectionslistmethods
         //step 35 to theater class, class seat
+
+        //instance to create object (from original program)
+        Theater theater = new Theater("Severance Hall", 8, 12);
+        //step 38 instance for list
+        List<Theater.Seat> seatCopy = new ArrayList<>(theater.seats);
+
+        printList(seatCopy);
+        //then run, printed seats horizontally A01 to H12
+
+        //step 39 reserve a seat
+        seatCopy.get(1).reserve(); //using copy of arraylist to reserve seat
+        if (theater.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
+        } else {
+            System.out.println("Seat is already reserves");
+            //run, showing that 2 separate arraylists sharing the same data, seat object
+            /*Seat A02 is reserved
+            .......Seat is already reserves*/
+        }
 
     }
 
